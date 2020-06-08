c3430e3f9e6709205af0d1bf730e532b058b5509
Paul Devlin <Paul.Devlin@nihr.ac.uk>
2020-06-08 11:21:36 +0100

    initial commit

---
+++ src/main/java/com/contrast/demo/RestdemoApiApplication.java
@@ -0,0 +1,13 @@
+package com.contrast.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestdemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestdemoApiApplication.class, args);
+	}
+
+}
