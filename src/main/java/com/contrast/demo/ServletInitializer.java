901c91ebf8a26b2ba30fabca04ed140a1b124df2
Paul Devlin <Paul.Devlin@nihr.ac.uk>
2020-06-08 12:12:59 +0100

    Config Updates, data.sql load and Base working API setup, also ripped out tests

--- src/main/java/com/contrast/demo/ServletInitializer.java
+++ src/main/java/com/contrast/demo/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(RestdemoApiApplication.class);
+		return application.sources(JPAApiDemo.class);
 	}
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Orgs(name) VALUES('PA');
+INSERT INTO Orgs(name) VALUES('Boeing');
+INSERT INTO Orgs(name) VALUES('Contrast');
+INSERT INTO Orgs(name) VALUES('Liberty');
+
+COMMIT;
